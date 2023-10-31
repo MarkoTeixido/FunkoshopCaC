34a4bd28b8fbd739e9535257f026cf7bdab7a2a6
MarkoTeixido <teixido_marko@outlook.es>
2023-10-30 22:04:54 -0300

    menu-toggle.js terminado

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,34 +1,62 @@
+//Flag variable for lockedScroll function
 let scrollLock = true;
 
+//Function Burger Toggle Menu
 function toggleMenu(){
+    //Selecting Items and Saving Them in Variables
     let menuButton = document.querySelector(".navbar-bars");
     let menu = document.querySelector(".navbar-menu");
+    let submenuButton = document.querySelector(".fa-solid.fa-chevron-down");
+    let submenu = document.querySelector(".submenu");
     let barsIcon = document.querySelector(".fa-solid.fa-bars.fa-2xl");
-
+    
+    //Condition to apply the Toggle Menu functionality only for tablet and mobile
     if(window.innerWidth < 1000){
-        toggleAction();
+        toggleMenuAction(); 
     }
     else{
         return 0;
     }
 
-    function toggleAction(){
+    //Menu Toggle functionality
+    function toggleMenuAction(){
+        //Listening to "click" event to enable/disable the Menu
         menuButton.addEventListener("click", function(){
             if(menu.classList.contains("inactive")){
                 menu.classList.remove("inactive");
                 menu.classList.add("active");
+                toggleSubmenuAction();
                 toggleIcon();
-                LockedScroll();
+                lockedScroll();
             }
             else{
                 menu.classList.remove("active");
                 menu.classList.add("inactive");
+                toggleSubmenuAction();
                 toggleIcon();
-                LockedScroll();
+                lockedScroll();
             }
         });
     }
 
+    //Submenu Toggle functionality
+    function toggleSubmenuAction(){
+        //Listening to "click" event to enable/disable the Submenu
+        submenuButton.addEventListener("click", function(){
+            if(submenu.style.display === "none" || submenu.style.display === ""){
+                submenu.style.display = "block";
+                submenuButton.className= "";
+                submenuButton.classList.add("fa-solid", "fa-chevron-up");
+            }
+            else{
+                submenu.style.display = "none";
+                submenuButton.className= "";
+                submenuButton.classList.add("fa-solid", "fa-chevron-down");
+            }
+        });
+    }
+
+    //Toggle functionality of Icon Bars and XMark
     function toggleIcon(){
         if(barsIcon.classList.contains("fa-bars")){
             barsIcon.className= "";
@@ -40,7 +68,8 @@ function toggleMenu(){
         }
     }
 
-    function LockedScroll(){
+    //Functionality to block/unlock scrolling
+    function lockedScroll(){
         if(scrollLock){
             document.body.style.overflow = "hidden";
         }
@@ -52,5 +81,6 @@ function toggleMenu(){
      
 }
 
+//Event Handlers for Site Size and Load
 window.addEventListener("resize", toggleMenu);
 window.addEventListener("load", toggleMenu);
